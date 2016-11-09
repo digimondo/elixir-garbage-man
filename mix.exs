69e2052704f3246f9eead8e78dc21e403b57b64e
Julius Beckmann <julius.beckmann@digimondo.de>
2016-11-09 13:56:16 +0100

    Fix and release v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GarbageMan.Mixfile do
   def project do
     [
       app: :garbage_man,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
